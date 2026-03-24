8d15feb6b878f165c3b6c861f9a219df620f9b5f
Sneha Tripathi <st0744397@gmail.com>
2026-03-24 23:44:54 +0530

    ems fronend design

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../other/Header'
 import TaskListnumber from '../other/taskListnumber'
+import Tasklist from '../Tasklist/Tasklist'
 
 const EmployeeDashboard = () => {
   return (
     <div className='p-10 bg-#222 h-screen'>
       <Header/>
-      <TaskListnumber/>git branch -M main
+      <TaskListnumber/>
+      <Tasklist/>
 
     </div>
   )
--- src/components/Tasklist/Tasklist.jsx
+++ src/components/Tasklist/Tasklist.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Tasklist = () => {
   return (
-    <div>
+    <div id='tasklist' className=' overflow-x-auto flex flex-nowrap items-center justify-start gap-5 h-[55%] w-full py-5 mt-10'>
+      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
+      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
+      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
+      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
+      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
+      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
+      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
       
     </div>
   )
