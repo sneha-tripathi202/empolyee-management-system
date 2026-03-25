72518e86afb80359b2ee7bde91c4faba01a1ac59
Sneha Tripathi <st0744397@gmail.com>
2026-03-25 17:24:38 +0530

    employee dashboard

--- src/components/Tasklist/Tasklist.jsx
+++ src/components/Tasklist/Tasklist.jsx
@@ -3,13 +3,30 @@ import React from 'react'
 const Tasklist = () => {
   return (
     <div id='tasklist' className=' overflow-x-auto flex flex-nowrap items-center justify-start gap-5 h-[55%] w-full py-5 mt-10'>
-      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
-      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
-      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
-      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
-      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
-      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
-      <div className='h-full shrink-0 w-75 bg-yellow-400 rounded-xl '></div>
+      <div className='h-full shrink-0 w-75 p-5 bg-yellow-400 rounded-xl items-center '>
+       <div className='flex justify-between'> <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+        <h4 className='text-sm'> 20 feb 2024</h4>
+        </div><h2 className='mt-5 text-2xl font-semibold'>make a youtube video</h2>
+      <p className='text-sm mt-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis debitis dignissimos earum iste voluptates asperiores.</p>
+      </div>
+       <div className='h-full shrink-0 w-75 p-5 bg-blue-400 rounded-xl items-center '>
+       <div className='flex justify-between'> <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+        <h4 className='text-sm'> 20 feb 2024</h4>
+        </div><h2 className='mt-5 text-2xl font-semibold'>make a youtube video</h2>
+      <p className='text-sm mt-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis debitis dignissimos earum iste voluptates asperiores.</p>
+      </div>
+       <div className='h-full shrink-0 w-75 p-5 bg-red-400 rounded-xl items-center '>
+       <div className='flex justify-between'> <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+        <h4 className='text-sm'> 20 feb 2024</h4>
+        </div><h2 className='mt-5 text-2xl font-semibold'>make a youtube video</h2>
+      <p className='text-sm mt-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis debitis dignissimos earum iste voluptates asperiores.</p>
+      </div>
+       <div className='h-full shrink-0 w-75 p-5 bg-green-400 rounded-xl items-center '>
+       <div className='flex justify-between'> <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+        <h4 className='text-sm'> 20 feb 2024</h4>
+        </div><h2 className='mt-5 text-2xl font-semibold'>make a youtube video</h2>
+      <p className='text-sm mt-2'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis debitis dignissimos earum iste voluptates asperiores.</p>
+      </div>
       
     </div>
   )
